583e333974b53c13b1ca2ff7a9f8da823dd11884
Morgan Fudge <mfudge92@gmail.com>
2017-12-07 01:17:48 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
   name text,
   age int,
   gender char(1),
   color text,
   temperament text,
-  alive boolean,
-  id INTEGER PRIMARY KEY
+  alive boolean
 );
