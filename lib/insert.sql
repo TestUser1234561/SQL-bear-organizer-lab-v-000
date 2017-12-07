e04a73f8c9a4adf0ae50f94aa2ea101f9519d701
Morgan Fudge <mfudge92@gmail.com>
2017-12-07 00:52:08 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('1', 1, 0, 'black', 'cool', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('2', 12, 0, 'red', 'cool', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('3', 141, 1, 'black', 'cool', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('4', 11, 0, 'green', 'cool', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('5', 142, 0, 'black', 'cool', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('6', 11, 0, 'black', 'cool', true)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('7', 15, 1, 'blue', 'cool', true)
-INSERT INTO bears (age, gender, color, temperament, alive) VALUES (83, 0, 'cyan', 'cool', false)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('1', 1, 0, 'black', 'cool', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('2', 12, 0, 'red', 'cool', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('3', 141, 1, 'black', 'cool', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('4', 11, 0, 'green', 'cool', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('5', 142, 0, 'black', 'cool', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('6', 11, 0, 'black', 'cool', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('7', 15, 1, 'blue', 'cool', true);
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (83, 0, 'cyan', 'cool', false);
