3aaac634e384cda5729ba88570c1dd4309710ee0
Morgan Fudge <mfudge92@gmail.com>
2017-12-07 00:53:11 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,3 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('4', 11
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('5', 142, 0, 'black', 'cool', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('6', 11, 0, 'black', 'cool', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('7', 15, 1, 'blue', 'cool', true);
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (83, 0, 'cyan', 'cool', false);
